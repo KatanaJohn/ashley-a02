6afe20e0bff99fe96b3678346fd70682ab9939fe
John Ashley <johnashleyworkemail@gmail.com>
2021-09-18 22:26:20 -0400

    removed accidental duplicate files

--- exercise16/src/main/java/ageCheck.java
+++
@@ -1,9 +0,0 @@
-public class ageCheck {
-
-    public void check(int age)
-    {
-        String result = (age >= 16) ? "are" : "are not";
-
-        System.out.printf("You %s old enough to legally drive.", result);
-    }
-}
